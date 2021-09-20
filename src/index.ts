181bad1bad7245d7fd8fabf31e2746b9659f646f
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2021-09-20 20:06:55 +0800

    node mvc init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const app = express();
+
+console.log( path.resolve(__dirname, `./environments/${ process.env.NODE_ENV }.env`));
+
+// 動態選擇環境變數的檔案
+dotenv.config({ path: path.resolve(__dirname, `./environments/${ process.env.NODE_ENV }.env`) });
+
+// console.log(process.env);
+
+app.get('/', (req, res, next) => {
+    res.send('Hello, World!!');
+});
+
+app.listen(process.env.PORT, () => console.log(`http server is running at port ${ process.env.PORT }.`));
